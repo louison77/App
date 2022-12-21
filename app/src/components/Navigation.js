1af8c7273fddd1670fdb50bfcc3756a0980b5414
Louison <Louison7777@gmail.com>
2022-12-21 18:02:53 +0100

    change pt4

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+//import { NavLink } from 'react-router-dom';
 const Navigation = () => {
     return (
         <div className="navigation">
-            <ul>
-                <NavLink to="/">
-                    <li>Accueil</li>
-                </NavLink>
-            </ul>
+            
+            
         </div>
     );
 };
