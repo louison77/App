abfb8af124dcf814764912d303a43781253c10c0
Louison <Louison7777@gmail.com>
2022-12-21 17:55:06 +0100

    change pt2

--- app/src/App.js
+++ app/src/App.js
@@ -6,13 +6,23 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="*" element={<Home />} />
-  </Routes>
-  </BrowserRouter> );
-  
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
