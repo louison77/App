c2ed7852bf4dc654404d7ea4292353c829e0c524
Louison <Louison7777@gmail.com>
2023-01-02 18:26:09 +0100

    test

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route  } from 'react-router-dom';
 import './App.css';
 import Exigences from './components/Exigences';
 import Export from './components/Export';
-import FormExigence from './components/FormExigence';
+
 import Mesures from './components/Mesures';
 import Suivi from './components/Suivi';
 import Accueil from './pages/Accueil';
