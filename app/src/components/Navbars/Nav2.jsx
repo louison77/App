aa4ede9c94b2bc2421a978180907ca5dc214db9d
Louison <Louison7777@gmail.com>
2023-01-12 17:52:47 +0100

    Import name projet

--- app/src/components/Navbars/Nav2.jsx
+++ app/src/components/Navbars/Nav2.jsx
@@ -4,7 +4,8 @@ import Button from "react-bootstrap/Button";
 import "../../styles/components/NavbarsStyle/_nav2.css";
 import BoutonProfil from "./BoutonProfil";
 
-const Nav2 = () => {
+const Nav2 = (props) => {
+  const nom = props.name;
   return (
     <div>
       <div className="navbar2">
@@ -23,24 +24,24 @@ const Nav2 = () => {
             </Button>
           </Link>
 
-          <span className="NameProject"> Project Name</span>
+          <span className="NameProject"> Projet : {nom}</span>
         </div>
 
         <div className="menu">
           <ul className="menu-liste">
-            <Link to="/Gestion/Resume" className="Menu-link">
+            <Link to={"/Gestion/" + nom + "/Resume"} className="Menu-link">
               <li>Synthèse</li>
             </Link>
-            <Link to="/Gestion/Exigences" className="Menu-link">
+            <Link to={"/Gestion/" + nom + "/Exigences"} className="Menu-link">
               <li>Exigences</li>
             </Link>
-            <Link to="/Gestion/Mesures" className="Menu-link">
+            <Link to={"/Gestion/" + nom + "/Mesures"} className="Menu-link">
               <li>Plan d'actions</li>
             </Link>
-            <Link to="/Gestion/Suivi" className="Menu-link">
+            <Link to={"/Gestion/" + nom + "/Suivi"} className="Menu-link">
               <li>Suivi</li>
             </Link>
-            <Link to="/Gestion/Export" className="Menu-link">
+            <Link to={"/Gestion/" + nom + "/Export"} className="Menu-link">
               <li>Export</li>
             </Link>
           </ul>
