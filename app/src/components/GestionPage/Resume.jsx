55c949a3e21efdae5a01582ce0a953caf16bce00
boyan <be.cieutat@protonmail.com>
2023-01-18 14:40:53 +0100

    Working on the tiled layout of graphs on Resume page.

--- app/src/components/GestionPage/Resume.jsx
+++ app/src/components/GestionPage/Resume.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import "../../styles/components/GestionStyle/_resume.css";
 
 const Resume = () => {
   return (
-    <div>
+    <div class="container">
       <h1>Synthèse</h1>
+      <div class="first">tile1</div><div class="third">tile2</div>
+      <div class="second">tile3</div><div class="fourth">tile4</div><div class="last">last</div>
     </div>
   );
+
 };
 
 export default Resume;
