0dc917538625691635a03cd4c7c9ac554fec428a
boyan <be.cieutat@protonmail.com>
2023-01-18 15:01:08 +0100

    Updating graphs on Resume

--- app/src/components/GestionPage/Resume.jsx
+++ app/src/components/GestionPage/Resume.jsx
@@ -5,8 +5,11 @@ const Resume = () => {
   return (
     <div class="container">
       <h1>Synthèse</h1>
-      <div class="first">tile1</div><div class="third">tile2</div>
-      <div class="second">tile3</div><div class="fourth">tile4</div><div class="last">last</div>
+      <div class="first">tile1</div>
+      <div class="third">tile2</div>
+      <div class="second">tile3</div>
+      <div class="fourth">tile4</div>
+      <div class="last">last</div>
     </div>
   );
 
