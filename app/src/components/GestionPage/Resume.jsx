93a10be09faf7668362d6e79c99f4270efbdcb59
Louison <Louison7777@gmail.com>
2023-01-24 13:03:36 +0100

    Chnage authentication

--- app/src/components/GestionPage/Resume.jsx
+++ app/src/components/GestionPage/Resume.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import "../../styles/components/GestionStyle/_resume.css";
-import RadarChart from "./Graphs/ProgressRadar.jsx"
-import PolarAreaChart from "./Graphs/CostPolarArea.jsx"
-import ProgressBar from "./Graphs/ProgressBar";
+import RadarChart from "./Graphs/ProgressRadar.jsx";
+import PolarAreaChart from "./Graphs/CostPolarArea.jsx";
+//import ProgressBar from "./Graphs/ProgressBar";
 
 const Resume = () => {
   return (
     <div class="container">
       <h1>Synthèse</h1>
-      <div class="tile_two-third_left"> 
-      <h3>Avancement</h3> 
-      <div class="tile_half_left"><RadarChart/></div>
-      <div class="tile_half_right">Progress Bar here</div> 
+      <div class="tile_two-third_left">
+        <h3>Avancement</h3>
+        <div class="tile_half_left">
+          <RadarChart />
+        </div>
+        <div class="tile_half_right">Progress Bar here</div>
       </div>
 
-      <div class="tile_one-third_right"> 
-      <h3>Coûts</h3>
-      <PolarAreaChart/>
+      <div class="tile_one-third_right">
+        <h3>Coûts</h3>
+        <PolarAreaChart />
       </div>
 
-      <div class="tile_full"> 
-      <h3>Options</h3>
+      <div class="tile_full">
+        <h3>Options</h3>
       </div>
-
 
       {/*
       <div class="tile_full"> full </div>
@@ -35,7 +36,6 @@ const Resume = () => {
       */}
     </div>
   );
-
 };
 
 export default Resume;
