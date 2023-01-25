8e581cb6c30335bdfee941fb95d5d250342bc829
Louison <Louison7777@gmail.com>
2023-01-26 00:27:12 +0100

    Bdd

--- api/hello/index.ts
+++ api/hello/index.ts
@@ -22,24 +22,3 @@ async function createOne(projetData) {
     return projet.save();
 }
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-
-
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + " cc" + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-            input: name,
-            message: responseMessage
-        }
-    };
-
-};
-
-
-export default httpTrigger;
