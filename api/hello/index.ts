ad08ce83d81eb3a23e12a44bc53a2328c81d45b5
Louison <Louison7777@gmail.com>
2023-01-25 22:41:32 +0100

    add excel

--- api/hello/index.ts
+++ api/hello/index.ts
@@ -2,9 +2,6 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const XLSX = require('xlsx')
-    const wb = XLSX.readFile('../Excel/Exigences.xlsx')
-    console.log(wb)
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + " cc" + ". This HTTP triggered function executed successfully."
