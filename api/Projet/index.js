cca0c6face9b53a72b8ce282fef55fb1f69efe56
ArthurVaret <arthur.varet@edu.devinci.fr>
2023-02-06 12:14:54 +0100

    manager & auditeur

--- api/Projet/index.js
+++ api/Projet/index.js
@@ -1,17 +1,76 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+const mongoose = require('mongoose');
+console.log(mongoose)
+// Connect to the database
+mongoose.connect(
+    process.env.CONNECTION_STRING, { // boiler plate values
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+);
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const ProjetSchema = new mongoose.Schema({
+    projetid: { type: String, unique: true, required: true },
+    statutaudit: String,
+    statutplanaction: String,
+    manager: String,
+    auditeur: String
+  });
+  
+  const Projet = mongoose.model("Projet", ProjetSchema);
 
+// Export our function
+module.exports = async function (context, req) {
+    // setup our default content type (we always return JSON)
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        header: {
+            "Content-Type": "application/json"
+        }
+    }
 
+    // Read the method and determine the requested action
+    switch (req.method) {
+        // If get, return all tasks
+        case 'GET':
+            await findAll(context);
+            break;
+        // If post, create new task
+        case 'POST':
+            await createOne(context);
+            break;
+        // If put, update task
+        case 'PATCH':
+            await updateOne(context);
+            break;
+        case 'DELETE':
+            await deleteOne(context);
+            break;
+    }
 };
 
-export default httpTrigger;
+function findAll() {
+    return Projet.find();
+}
+
+async function createOne(projetData) {
+    const projet = new Projet(projetData);
+    return projet.save();
+}
+
+async function updateOne(id, projetData) {
+    const projet = await findOne(id);
+    for (const projetElementKey in projetData) {
+      if (
+        projetElementKey[0] !== "_" &&
+        projetData.hasOwnProperty(projetElementKey)
+      ) {
+        projet[projetElementKey] = projetData[projetElementKey];
+      }
+    }
+    await projet.save();
+    return await findOne(id);
+}
+
+async function deleteOne(id) {
+    const projet = await findOne(id);
+    return projet.remove();
+}
