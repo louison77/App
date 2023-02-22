a9fb9c1c3c66671984515ff46a88b0a5fe5b0205
Louison <Louison7777@gmail.com>
2023-02-22 16:42:12 +0100

    Change Button add auditeur

--- app/src/components/GestionPage/Suivi.jsx
+++ app/src/components/GestionPage/Suivi.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import { useOutletContext } from "react-router-dom";
 import "../../styles/components/GestionStyle/_suivi.css";
+import Combobox from "react-widgets/Combobox";
+import "react-widgets/styles.css"
 
 const Suivi = () => {
     const [code] = useOutletContext();
     const baseUrl = "/api/Projet"
     const baseUrl2 = "/api/Utilisateur"
+    const [alluser, setalluser] = useState([])
     const [newauditeur, setauditeur] = useState("");
     const [auditeursprojet, setauditeursprojet] = useState([]);
     const [Refresh, setRefresh] = useState("");
@@ -30,17 +33,31 @@ const Suivi = () => {
             catch (error) {
                 console.log(error)
             }
+            try {
+                const response = await axios.get(`${baseUrl2}`)
+                const retrievedUser = response.data.users;
+                const tab = []
+                retrievedUser.forEach(element => {
+                    if (element.role === "Auditeur" || element.role === "Manager") {
+
+                        tab.push(element.mail)
+
+                    }
+
+                })
+                setalluser(tab)
+            }
+            catch (error) {
+
+            }
 
         }
         getProject()
     }, [code, Refresh])
-    const handleChange = (event) => {
-        setauditeur(event.target.value);
-    };
     const handleSubmit = (event) => {
         event.preventDefault();
         const sendauditeur = async () => {
-            if (newauditeur !== "") {
+            if (newauditeur !== "" && (!auditeursprojet.includes(newauditeur))) {
                 const response = await axios.get(`${baseUrl}`)
                 const retrievedProject = response.data.projets;
                 var tab = []
@@ -114,19 +131,29 @@ const Suivi = () => {
         }
         deleteA()
     }
+
     return (
         <div>
 
             <div className='AjoutArea'>
                 <h2> Gestion auditeurs du projet : </h2>
                 <form className="FormAuditeur" action="submit" onSubmit={handleSubmit}>
-                    <input className='AuditeurInput' value={newauditeur} type="text"
-                        placeholder="Ajouter un nouvel auditeur au projet"
-                        onChange={handleChange}>
-                    </input>
+
+                    <Combobox
+                        //busy={setalluser}
+                        className='Combobutton'
+                        data={alluser}
+                        textField="mail"
+                        value={newauditeur}
+                        onChange={setauditeur}
+                        hideEmptyPopup
+                    //hideCaret
+                    />
                     <button className="Addauditeur">Nouvel auditeur</button>
 
                 </form>
+
+
             </div>
             <div className='TableAuditeurs'>
                 <table>
